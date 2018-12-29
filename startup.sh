7e9ca46b00095d5b905c3bef660afd9f777b4378
Feng <673319230@qq.com>
2018-12-29 11:39:59 +0800

    Commit

--- startup.sh
+++ startup.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env sh
-workspace='.'
 filename='fengleicn_dht'
 
 git pull
 mvn package
-mkdir ${workspace} -p
-cp target/${filename}.jar ${workspace}
-cd ${workspace}
+
 pid=$( ps -ef | grep ${filename} | grep jar | awk '{print $2}' )
 if [ -n "$pid" ]; then
     kill -9 ${pid}
 fi
-nohup java -Xmx512m -Xms512m -jar ${filename}.jar &>err.log &
+
+nohup java -Xmx512m -Xms512m -jar target/${filename}.jar &>err.log &
