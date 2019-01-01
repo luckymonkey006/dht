fe6434282530eab954329bf538b4361ef6678638
Feng <673319230@qq.com>
2019-01-01 23:45:20 +0800

    commit

--- startup.sh
+++ startup.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env sh
+
+check_process(){
+    if [ "$1" = "" ];
+    then
+        return 1
+    fi
+
+    process_num=`ps -ef | grep "$1" | grep -v "grep" | wc -l`
+
+    if [ $process_num -eq 1 ];
+    then
+        return 1
+    else
+        return 0
+    fi
+}
+
 jar_name='fengleicn_dht'
 id='2f12a82'
 
@@ -12,11 +29,11 @@ fi
 
 nohup java -Xmx512m -Xms512m -jar target/${filename}.jar ${id} &>err.log &
 
-#while(true)
-#do
-#  sleep 5
-#  pid=$( ps -ef | grep ${jar_name} | grep ${id} | awk '{print $2}' )
-#  if [ -z "$pid" ]; then
-#        nohup java -Xmx512m -Xms512m -jar target/${filename}.jar ${id} &>err.log &
-#  fi
-#done
+nohup while true
+do
+  sleep 5
+  check_process ${id}
+  if [ $? -eq 0 ]; then
+        nohup java -Xmx512m -Xms512m -jar target/${filename}.jar ${id} &>err.log &
+  fi
+done &>/dev/null &
