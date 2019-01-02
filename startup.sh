942829afa5ac5aeb349caa8c5bf21ea4599d7c70
Feng <673319230@qq.com>
2019-01-02 09:45:47 +0800

    Commit

--- startup.sh
+++ startup.sh
@@ -1,39 +1,22 @@
 #!/usr/bin/env sh
-
-check_process(){
-    if [ "$1" = "" ];
-    then
-        return 1
-    fi
-
-    process_num=`ps -ef | grep "$1" | grep -v "grep" | wc -l`
-
-    if [ $process_num -eq 1 ];
-    then
-        return 1
-    else
-        return 0
-    fi
-}
-
 jar_name='fengleicn_dht'
 id='2f12a82'
+cmd="nohup java -Xmx512m -Xms512m -jar target/${filename}.jar ${id} &>err.log &"
+get_pid="ps -ef | grep ${jar_name} | grep ${id} | awk '{print $2}'"
 
 git pull
 mvn package
-
-pid=$( ps -ef | grep ${jar_name} | grep ${id} | awk '{print $2}' )
+pid=$(${get_pid})
 if [ -n "$pid" ]; then
     kill -9 ${pid}
 fi
-
-nohup java -Xmx512m -Xms512m -jar target/${filename}.jar ${id} &>err.log &
-
-#while true
-#do
-#  sleep 5
-#  check_process ${id}
-#  if [ $? -eq 0 ]; then
-#        nohup java -Xmx512m -Xms512m -jar target/${filename}.jar ${id} &>err.log &
-#  fi
-#done &>/dev/null &
+${cmd}
+while true
+do
+  sleep 5
+  pid=$(${get_pid})
+  if [ -z "$pid" ]; then
+      ${cmd}
+  fi
+done &
+disown -a
