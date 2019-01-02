ec085c132d2d0d8a800186345e2abd6ab14e9dcf
Feng <673319230@qq.com>
2019-01-02 10:34:45 +0800

    Commit

--- startup.sh
+++ startup.sh
@@ -2,11 +2,10 @@
 jar_name='fengleicn_dht'
 id='2f12a82hashcode2f12a82'
 cmd="nohup java -Xmx512m -Xms512m -jar target/${filename}.jar ${id} &>err.log &"
-get_pid="ps -ef | grep ${jar_name} | grep ${id} | awk '{print $2}'"
 
 git pull
 mvn package
-pid=$(${get_pid})
+pid=$( ps -ef | grep ${jar_name} | grep ${id} | awk '{print $2}' )
 if [ -n "$pid" ]; then
     kill -9 ${pid}
 fi
@@ -14,7 +13,7 @@ ${cmd}
 while true
 do
   sleep 5
-  pid=$(${get_pid})
+  pid=$( ps -ef | grep ${jar_name} | grep ${id} | awk '{print $2}' )
   if [ -z "$pid" ]; then
       ${cmd}
   fi
