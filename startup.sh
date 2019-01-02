504ce8763b190219eca9c8812d6c766f03a08831
Feng <673319230@qq.com>
2019-01-02 14:46:26 +0800

    Commit

--- startup.sh
+++ startup.sh
@@ -4,21 +4,21 @@ cmd="nohup java -Xmx512m -Xms512m -jar target/${jar_name}.jar 1>err.log 2>&1 &"
 
 git pull
 mvn package
-pid=$( ps -ef | grep startup | grep sh | awk '{print $2}' )
-if [ -n "$pid" ]; then
-    kill -9 ${pid}
-fi
+
 pid=$( ps -ef | grep ${jar_name} | grep java | awk '{print $2}' )
 if [ -n "$pid" ]; then
     kill -9 ${pid}
 fi
 ${cmd}
-while true
-do
-  sleep 5
-  pid=$( ps -ef | grep ${jar_name} | grep java | awk '{print $2}' )
-  if [ -z "$pid" ]; then
-      ${cmd}
-  fi
-done &
-disown -a
+
+sh_pid_num=$( ps -ef | grep startup | grep sh | awk '{print $2}' | wc -l )
+if [ "$sh_pid_num" -le 1 ]; then
+    while true; do
+        sleep 5
+        pid=$( ps -ef | grep ${jar_name} | grep java | awk '{print $2}' )
+        if [ -z "$pid" ]; then
+            ${cmd}
+        fi
+    done &
+    disown -a
+fi
