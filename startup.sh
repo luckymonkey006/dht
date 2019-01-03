2228fab614274e5f2156320fc9b5c6984ec60d3d
Feng <673319230@qq.com>
2019-01-03 16:36:12 +0800

    Commit

--- startup.sh
+++ startup.sh
@@ -21,7 +21,6 @@ if [[ -n "$pid" ]]; then
     kill -9 ${pid}
 fi
 bash -c "${cmd}"
-echo $( ps -ef | grep startup | grep sh | awk '{print $0}' )
 sh_pid_num=$( ps -ef | grep dht_demon_do_while | grep -v grep | awk '{print $2}' | wc -l )
 if [[ "$sh_pid_num" -le 1 ]]; then
     dht_demon_do_while
