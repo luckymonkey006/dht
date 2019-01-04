d55a248f217091271dd1c22470fbaf2429791b7e
Feng <673319230@qq.com>
2019-01-04 10:25:44 +0800

    Commit

--- startup.sh
+++ startup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
-jar_name="fengleicn_dht"
-cmd="nohup java -Xmx512m -Xms512m -jar target/${jar_name}.jar 1>err.log 2>&1 &"
+jar_name="fengleicn_dht.jar"
+run_command="nohup java -Xmx512m -Xms512m -jar target/${jar_name}.jar 1>err.log 2>&1 &"
 dont_dead_protect(){
     while true; do
             sleep 5
-            pid=$( ps -ef | grep ${jar_name} | grep java | awk '{print $2}' )
+            pid=$(ps -ef | grep ${jar_name} | grep -v grep | awk '{print $2}')
             if [[ -z "$pid" ]]; then
-                bash -c "${cmd}"
+                bash -c "${run_command}"
             fi
         done &
     disown -a
@@ -15,13 +15,13 @@ dont_dead_protect(){
 git pull
 mvn package
 
-pid=$(ps -ef | grep ${jar_name} | grep java | awk '{print $2}')
+pid=$(ps -ef | grep ${jar_name} | grep -v grep | awk '{print $2}')
 if [[ -n "$pid" ]]; then
     kill -9 ${pid}
     has_old_process="true"
 fi
 
-bash -c "${cmd}"
+bash -c "${run_command}"
 if [[ -z "$has_old_process" ]]; then
     dont_dead_protect
 fi
